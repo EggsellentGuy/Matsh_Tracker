5ece38b12220af9e6d038156fe739ba0b5084662
EggsellentGuy <ivanrossi112@gmail.com>
2026-07-15 14:16:42 +0300

    add players list page

--- league/views.py
+++ league/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Player
 
-# Create your views here.
+
+def player_list(request):
+    players = Player.objects.all()
+    return render(
+        request,
+        "league/player_list.html",
+        context={"players": players},
+    )
